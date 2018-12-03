d21fe09259a385af2076c3efa5cdc67180552eae
Philippe Garneau <philippegarneau1@gmail.com>
2018-12-03 12:57:03 -0500

    Updated for dockerization

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,6 @@
 var config = {}
 
 config.port = process.env.CONFIGURATION_WEB_APP_PORT || 4000
-config.frontendUrl = process.env.FRONTEND_URL || 'http://localhost:4200'
 config.configurationServiceUrl = process.env.CONFIGURATION_SERVICE_URL || 'http://localhost:8081'
 
 module.exports = config
--- frontend/src/app/configuration.service.ts
+++ frontend/src/app/configuration.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ConfigurationService {
-  uri = 'http://localhost:4000';
+  uri = 'http://configuration-web-app-bff:4000';
 
   constructor(private http: HttpClient) { }
 
